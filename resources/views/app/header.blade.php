2229938f53ae840861e28cd5d39ce32c955505a2
Shweta Bansal <ershwetabansal@gmail.com>
2016-11-08 15:17:00 +0000

    Use proper header

--- resources/views/app/header.blade.php
+++ resources/views/app/header.blade.php
@@ -2,18 +2,6 @@
     <div class="container">
         <div class="navbar-header">
 
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Disk browser') }}
-            </a>
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
@@ -24,11 +12,18 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
+                                <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>
                     <li><a href="{{ url('/register') }}">Register</a></li>
-                @else
+                @endif
+                <li>
+                    <a href="{{ url('/contact') }}">Contact us</a>
+                </li>
+                <li>
+                    <a href="{{ url('/') }}">Track order</a>
+                </li>
+                @if (!Auth::guest())
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
